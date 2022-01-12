50ae93736a9922d4facd11b8cbc6c485c439ae6e
seijiro <modeverv@gmail.com>
2022-01-12 21:55:56 +0900

    2022-01-12 21:55:56

--- lib/story.dart
+++ lib/story.dart
@@ -1,5 +1,17 @@
-//TODO: Step 2 - Create a new class called Story.
+//DONE: Step 2 - Create a new class called Story.
+class Story {
+//DONE: Step 3 - Create 3 properties for this class,
+// A. storyTitle to store the story text.
+// B. choice1 to store the text for choice 1,
+// C. choice2 to store the text for choice 2.
+  String storyTitle;
+  String choice1;
+  String choice2;
 
-//TODO: Step 3 - Create 3 properties for this class, A. storyTitle to store the story text. B. choice1 to store the text for choice 1, C. choice2 to store the text for choice 2.
-
-//TODO: Step 4 - Create a Constructor for this class to be able to initialise the properties created in step 3.
+  //DONE: Step 4 - Create a Constructor for this class to be able to initialise the properties created in step 3.
+  Story({storyTitle: String, choice1: String, choice2: String}) {
+    this.storyTitle = storyTitle;
+    this.choice1 = choice1;
+    this.choice2 = choice2;
+  }
+}
