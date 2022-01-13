39a69f79fcefbb079884da9dc0249c534ec71937
seijiro <modeverv@gmail.com>
2022-01-13 12:57:05 +0900

    2022-01-13 12:57:05

--- lib/main.dart
+++ lib/main.dart
@@ -62,8 +62,8 @@ class _StoryPageState extends State<StoryPage> {
                   },
                   color: Colors.red,
                   child: Text(
-                    //TODO: Step 13 - Use the storyBrain to get the text for choice 1.
-                    'Choice 1',
+                    // DONE: Step 13 - Use the storyBrain to get the text for choice 1.
+                    storyBrain.getChoice1(),
                     style: TextStyle(
                       fontSize: 20.0,
                     ),
@@ -84,8 +84,8 @@ class _StoryPageState extends State<StoryPage> {
                   },
                   color: Colors.blue,
                   child: Text(
-                    //TODO: Step 14 - Use the storyBrain to get the text for choice 2.
-                    'Choice 2',
+                    // DONE: Step 14 - Use the storyBrain to get the text for choice 2.
+                    storyBrain.getChoice2(),
                     style: TextStyle(
                       fontSize: 20.0,
                     ),
